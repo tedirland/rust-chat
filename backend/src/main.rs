f7af951e1648a23b753b5953e92595e0468a83e3
tedirland <irlandth@gmail.com>
2024-02-21 13:48:57 -0600

    created a websocket connection to run on the /get route

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,18 @@
+use rocket::futures::{SinkExt, StreamExt};
+use rocket_ws::{Channel, WebSocket};
+
 #[rocket::get("/")]
-fn chat() {}
+fn chat(ws: WebSocket) -> Channel<'static> {
+    ws.channel(move |mut stream| {
+        Box::pin(async move {
+            while let Some(message) = stream.next().await {
+                let _ = stream.send(message?).await;
+            }
+
+            Ok(())
+        })
+    })
+}
 
 #[rocket::main]
 async fn main() {
