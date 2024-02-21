0eab8465af7bccfb72ca98cf4c97fad3addc6144
tedirland <irlandth@gmail.com>
2024-02-21 14:10:49 -0600

    created ChatRoom state struct  and passed it into manage function so endpoint can access it

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,28 @@
-use rocket::futures::StreamExt;
-use rocket_ws::{Channel, WebSocket};
+use std::{
+    collections::HashMap,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+
+use rocket::{
+    futures::{stream::SplitSink, StreamExt},
+    State,
+};
+use rocket_ws::{stream::DuplexStream, Channel, Message, WebSocket};
+
+static USER_ID_COUNTER: AtomicUsize = AtomicUsize::new(1);
+
+#[derive(Default)]
+struct ChatRoom {
+    connections: HashMap<usize, SplitSink<DuplexStream, Message>>,
+}
 
 #[rocket::get("/")]
-fn chat(ws: WebSocket) -> Channel<'static> {
+fn chat(ws: WebSocket, state: &State<ChatRoom>) -> Channel<'static> {
     ws.channel(move |stream| {
         Box::pin(async move {
+            // this block represents when a user is connected
+            let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
+
             let (mut ws_sink, mut ws_stream) = stream.split();
             while let Some(message) = ws_stream.next().await {
                 // let _ = ws_sink.send(message?).await;
@@ -19,6 +37,7 @@ fn chat(ws: WebSocket) -> Channel<'static> {
 async fn main() {
     let _ = rocket::build()
         .mount("/", rocket::routes![chat])
+        .manage(ChatRoom::default())
         .launch()
         .await;
 }
