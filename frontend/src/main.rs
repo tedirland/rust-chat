e7cb5406127b23b0d3cf4841e86a17c85b8e788b
tedirland <irlandth@gmail.com>
2024-02-23 12:15:56 -0600

    Create yew state using hooks. Monitor state with use_effect_with to update dom each time a message is sent

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,16 +1,30 @@
 use yew::prelude::*;
+use yew_hooks::use_websocket;
 
 #[function_component]
 fn App() -> Html {
-    html! {
-        <>
-        <div id="chat">
-        <h1></h1>
-        <p></p>
+    let messages_handle = use_state(Vec::default);
+    let messages = (*messages_handle).clone();
 
-        </div>
-        <div></div>
-        </>
+    let ws = use_websocket("ws://127.0.0.1:8000".to_string());
+
+    let mut cloned_messages = messages.clone();
+
+    use_effect_with(ws.message, move |ws_message| {
+        if let Some(ws_msg) = &**ws_message {
+            cloned_messages.push(ws_msg.clone());
+            messages_handle.set(cloned_messages);
+        }
+    });
+
+    html! {
+    <ul id="chat">
+        {
+            messages.iter().map(|m| html!{<li>{m}</li>}).collect::<Html>()
+        }
+
+    </ul>
+
     }
 }
 
