3b6e96857f3fcfdf49fbc9e325752fe1fe182de7
tedirland <irlandth@gmail.com>
2024-02-25 09:53:58 -0600

    created backend logic to broadcast user list when a new websocket conn is added

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use chrono::Utc;
-use common::{ChatMessage, WebSocketMessage};
+use common::{ChatMessage, WebSocketMessage, WebSocketMessageType};
 use rocket::{
     futures::{stream::SplitSink, SinkExt, StreamExt},
     tokio::sync::Mutex,
@@ -41,7 +41,7 @@ impl ChatRoom {
             created_at: Utc::now().naive_utc(),
         };
         let websocket_message = WebSocketMessage {
-            message_type: common::WebSocketMessageType::NewMessage,
+            message_type: WebSocketMessageType::NewMessage,
             message: Some(chat_message),
             users: None,
         };
@@ -49,6 +49,26 @@ impl ChatRoom {
         let mut conns = self.connections.lock().await;
 
         // iterate through the connections
+        for (_key, conn) in conns.iter_mut() {
+            // send the message to each connection
+            let _ = conn
+                .send(Message::Text(json!(websocket_message).to_string()))
+                .await;
+        }
+    }
+
+    pub async fn broadcast_user_list(&self) {
+        let mut conns = self.connections.lock().await;
+        let mut users = vec![];
+        for (id, conn) in conns.iter_mut() {
+            // send the message to each connection
+            users.push(format!("User # {}", id));
+        }
+        let websocket_message = WebSocketMessage {
+            message_type: WebSocketMessageType::UsersList,
+            message: None,
+            users: Some(users),
+        };
         for (_key, conn) in conns.iter_mut() {
             // send the message to each connection
             let _ = conn
@@ -65,12 +85,14 @@ fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
             let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
             let (ws_sink, mut ws_stream) = stream.split();
             state.add_connection(user_id, ws_sink).await;
+            state.broadcast_user_list().await;
 
             // whenever another websocket sends a message
             while let Some(message) = ws_stream.next().await {
                 state.broadcast_message(message?, user_id).await;
             }
             state.remove_connection(user_id).await;
+            state.broadcast_user_list().await;
 
             Ok(())
         })
