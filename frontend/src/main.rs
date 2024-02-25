6f96b96c5e972c8f73f75383813b79fcae2f040d
tedirland <irlandth@gmail.com>
2024-02-24 19:21:56 -0600

    made changes to styling of rendered chat messages

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -46,24 +46,30 @@ fn App() -> Html {
 
     html! {
     <div class="container">
-        <div class="row">
-        <div class="list-group">
+
             {
                 messages.iter().map(|m| html!{
-                    <div class="list-group-item list-group-item-action">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5>{m.author.clone()}</h5>
-                            <small>{m.created_at.format("Sent at %l %p on %b %-d").to_string()}</small>
+                    // <div class="list-group-item list-group-item-action">
+                    //     <div class="d-flex w-100 justify-content-between">
+                    //         <h5>{m.author.clone()}</h5>
+                    //         <small>{m.created_at.format("Sent at %l %p on %b %-d").to_string()}</small>
+                    //     </div>
+                    //     <p>
+                    //     {m.message.clone()}
+                    // </p>
+                    // </div>
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">{m.author.clone()}</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">{m.created_at.format("Sent at %l %p on %b %-d").to_string()}</h6>
+                          <p class="card-text">{m.message.clone()}</p>
                         </div>
-                        <p>
-                        {m.message.clone()}
-                    </p>
-                    </div>
+                      </div>
+
                 }
                 ).collect::<Html>()
             }
-        </div>
-        </div>
+
         <div class="row">
                 <div class="input-group">
                 <textarea class="form-control" onchange={on_message_change} value={new_message}></textarea>
